0fd7c5cefd8de4e658f2857bc6d3a4bd4136f646
katakoto291 <kakkookakoo@gmail.com>
2023-04-02 17:05:41 +0900

    代入変数をダブルクオートで囲むよう修正

--- scripts/hasura-migrate.sh
+++ scripts/hasura-migrate.sh
@@ -4,4 +4,4 @@
 cd ../hasura || exit
 
 # マイグレーション実行とメタデータの適用
-npx hasura deploy --disable-interactive --endpoint ${_HASURA_GRAPHQL_ENDPOINT} --admin-secret ${_HASURA_GRAPHQL_ADMIN_SECRET}
+npx hasura deploy --disable-interactive --endpoint "${_HASURA_GRAPHQL_ENDPOINT}" --admin-secret "${_HASURA_GRAPHQL_ADMIN_SECRET}"
