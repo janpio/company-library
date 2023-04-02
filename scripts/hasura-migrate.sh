0a0810b73376ed06fe68f21e52c0001fae301076
katakoto291 <kakkookakoo@gmail.com>
2023-04-02 17:02:03 +0900

    代入変数を用いて指定するよう変更

--- scripts/hasura-migrate.sh
+++ scripts/hasura-migrate.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+
 # configファイルのあるディレクトリに移動
 cd ../hasura || exit
 
 # マイグレーション実行とメタデータの適用
-npx hasura deploy --disable-interactive
+npx hasura deploy --disable-interactive --endpoint ${_HASURA_GRAPHQL_ENDPOINT} --admin-secret ${_HASURA_GRAPHQL_ADMIN_SECRET}
